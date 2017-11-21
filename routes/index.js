041154f50a0ecdd993aa5f19670cbf6a7bc0ce0c
Hector <hector.j.manriquez@gmail.com>
2017-11-21 17:21:12 -0600

    Adding files

---
+++ routes/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/auth', (req, res, next) => {
+  console.log(req);
+  console.log(req.query);
+  res.status(200).json(req.params);
+});
+
+router.get('/access', (req, res, next) => {
+  console.log(req);
+  console.log(req.query);
+  res.status(200).json(req.query);
+});
+
+router.get('/test', (req, res, next) => {
+  res.status(200).send({
+      'status': 200,
+      'message': 'success'
+  })
+});
+
+router.post('/test', (req, res, next) => {
+    console.log(req);
+    res.status(200).send({
+        'status': 200,
+        'message': 'success'
+    })
+});
+
+module.exports = router;
